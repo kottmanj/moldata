41deca0b6ef50a74b1740b78bf8af21b0b429a2b
Jakob Kottmann <jakob.kottmann@gmail.com>
2021-09-29 15:42:08 -0400

    added larger beh2 dataset

--- script.py
+++ script.py
@@ -4,14 +4,14 @@ import numpy
 import sys
 import os
 
-def compute_molecule(points):
+def compute_molecule(points,n_pno=2):
     geometry="Be 0.0 0.0 0.0\nH 0.0 0.0 {R1:2.2f}\nH 0.0 0.0 -{R2:2.2f}"
     name="beh2_{R1:2.2f}_{R2:2.2f}_180"
     os.mkdir(name.format(**points))
     os.chdir(name.format(**points))
     R1 = points["R1"]
     R2 = points["R2"]
-    mol=tq.Molecule(name=name.format(**points), geometry=geometry.format(**points), n_pno=2)
+    mol=tq.Molecule(name=name.format(**points), geometry=geometry.format(**points), n_pno=n_pno)
     os.chdir("..")
 
 os.environ['MAD_NUM_THREADS'] = '12'
