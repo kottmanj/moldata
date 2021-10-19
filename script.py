4fe58b81f8dea1dd0ec55e198d9ab9a8debabca4
Jakob Kottmann <jakob.kottmann@gmail.com>
2021-10-19 16:28:39 -0400

    added beh2 12 qubits data with maxrank=npno//2 to force 3 orbitals per pair

--- script.py
+++ script.py
@@ -4,14 +4,14 @@ import numpy
 import sys
 import os
 
-def compute_molecule(points,n_pno=4):
+def compute_molecule(points,n_pno=6):
     geometry="Be 0.0 0.0 0.0\nH 0.0 0.0 {R1:2.2f}\nH 0.0 0.0 -{R2:2.2f}"
     name="beh2_{R1:2.2f}_{R2:2.2f}_180"
     os.mkdir(name.format(**points))
     os.chdir(name.format(**points))
     R1 = points["R1"]
     R2 = points["R2"]
-    mol=tq.Molecule(name=name.format(**points), geometry=geometry.format(**points), n_pno=n_pno)
+    mol=tq.Molecule(name=name.format(**points), geometry=geometry.format(**points), n_pno=n_pno, pno={"maxrank":n_pno//2})
     os.chdir("..")
 
 os.environ['MAD_NUM_THREADS'] = '12'
